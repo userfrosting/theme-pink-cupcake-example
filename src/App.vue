b2d41a04a2717ef3648ee9f5ec20a8589d3a3a20
Louis Charette <charette.louis@gmail.com>
2024-04-14 23:23:57 -0400

    Init Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <UFNavBar title="Pink Cupcake">
+    <UFNavBarItem to="/" label="Home" />
+    <UFNavBarItem to="/about" label="About" />
+    </UFNavBar>
+    <UFMainContent>
+        <RouterView />
+    </UFMainContent>  
+</template>
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+app.use(router)
+
+/** Setup Theme */
+import '@userfrosting/theme-pink-cupcake/style.less'
+import PinkCupcake from '@userfrosting/theme-pink-cupcake'
+app.use(PinkCupcake)
+
+
+app.mount('#app')
