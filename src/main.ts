bdf56f449c3cba836b3baa94c8cad45b4b8c077a
Louis Charette <charette.louis@gmail.com>
2024-04-20 16:12:20 -0400

    Update from local implementation

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,8 @@ const app = createApp(App)
 app.use(router)
 
 /** Setup Theme */
-import '@userfrosting/theme-pink-cupcake/style.less'
+import '@userfrosting/theme-pink-cupcake/less/main.less'
 import PinkCupcake from '@userfrosting/theme-pink-cupcake'
 app.use(PinkCupcake)
-
 
 app.mount('#app')
