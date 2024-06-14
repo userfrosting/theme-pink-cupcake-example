d84b3d4824bae1accb454d0d2c338e66b7e3b2a0
Louis Charette <charette.louis@gmail.com>
2024-06-13 22:28:24 -0400

    Fix font

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ]
+    plugins: [
+        vue(),
+    ],
+    server: {
+        fs: {
+            // Allow serving files from one level up to the project root.
+            // Required fonts located in `node_modules`.
+            allow: ['..']
+        },
+    },
 })
